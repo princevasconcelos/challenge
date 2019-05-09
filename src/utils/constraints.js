01e978623327ebdcc061de3e41d1c39d7210c6bd
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-05-09 05:22:48 -0300

    add darkerGray and largePhone

--- src/utils/constraints.js
+++ src/utils/constraints.js
@@ -8,11 +8,15 @@ const styles = {
     lightGray: '#D4D4D4',
     lighterGray: '#EAEAEA',
     darkGray: '#525252',
+    darkerGray: '#a9a9a9',
   },
   sizes: {
     default: '16px',
     small: '14px',
     xsmall: '11px',
+  },
+  screens: {
+    largePhone: '550px',
   },
 };
 
