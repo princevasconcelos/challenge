111d83024ea33cc661db64789ab34bf01e05e4d0
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-05-09 03:50:15 -0300

    add constraints and date to utils

---
+++ src/utils/constraints.js
@@ -0,0 +1,19 @@
+const styles = {
+  colors: {
+    black: '#333',
+    red: '#E86C60',
+    blue: '#3FA2F7',
+    cyan: '#59EBFF',
+    gray: '#C4C4C4',
+    lightGray: '#D4D4D4',
+    lighterGray: '#EAEAEA',
+    darkGray: '#525252',
+  },
+  sizes: {
+    default: '16px',
+    small: '14px',
+    xsmall: '11px',
+  },
+};
+
+export default styles;
---
+++ src/utils/date.js
@@ -0,0 +1,23 @@
+import datefns from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+const format = (date, token) => datefns.format(date, token, { locale: pt });
+
+const isToday = (date, today) => date.getDay() === today.getDay();
+
+const getTableDates = () => {
+  const dayRange = 3;
+  const today = new Date();
+  const calendar = datefns.eachDay(
+    datefns.subDays(today, dayRange),
+    datefns.addDays(today, dayRange),
+  );
+
+  return calendar.map(date => ({
+    week: isToday(date, today) ? 'HOJE' : format(date, 'dddd').split('-')[0],
+    day: format(date, 'DD'),
+    month: format(date, 'MMMM'),
+  }));
+};
+
+export default getTableDates;
